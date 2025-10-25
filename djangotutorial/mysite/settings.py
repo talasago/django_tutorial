6c30fc06c47c71251e6fa2522652b4602e9140f8
talasago <42575556+talasago@users.noreply.github.com>
2025-10-25 18:05:07 +0900

    feat: add custom admin base template for Polls administration interface

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -121,3 +121,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
